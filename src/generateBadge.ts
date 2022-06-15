f7b0da09305b5acb89549d516fc1b882e8f0445d
Shubham Jha <shubhamjha@sharechat.co>
2022-06-15 11:21:24 +0530

    updated style of svg

--- src/generateBadge.ts
+++ src/generateBadge.ts
@@ -2,21 +2,60 @@ import fs, { writeFileSync } from "fs";
 import { join } from "path";
 
 const svg = `
-<svg xmlns="http://www.w3.org/2000/svg" width="104" height="20">
-  <script/>
-  <linearGradient id="a" x2="0" y2="100%">
-    <stop offset="0" stop-color="#bbb" stop-opacity=".1"/>
-    <stop offset="1" stop-opacity=".1"/>
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  xmlns:xlink="http://www.w3.org/1999/xlink"
+  width="88"
+  height="20"
+  role="img"
+>
+  <linearGradient id="s" x2="0" y2="100%">
+    <stop offset="0" stop-color="#bbb" stop-opacity=".1" />
+    <stop offset="1" stop-opacity=".1" />
   </linearGradient>
-  <rect rx="3" width="104" height="20" fill="#555"/>
-  <rect rx="3" x="64" width="40" height="20" fill="@color@"/>
-  <path fill="@color@" d="M64 0h4v20h-4z"/>
-  <rect rx="3" width="104" height="20" fill="url(#a)"/>
-  <g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11">
-    <text x="32" y="15" fill="#010101" fill-opacity=".3">type</text>
-    <text x="32" y="14">type</text>
-    <text x="84" y="15" fill="#010101" fill-opacity=".3">@coverage@</text>
-    <text x="84" y="14">@coverage@</text>
+  <clipPath id="r">
+    <rect width="88" height="20" rx="3" fill="#fff" />
+  </clipPath>
+  <g clip-path="url(#r)">
+    <rect width="35" height="20" fill="#555" />
+    <rect x="35" width="53" height="20" fill="@color@" />
+    <rect width="88" height="20" fill="url(#s)" />
+  </g>
+  <g
+    fill="#fff"
+    text-anchor="middle"
+    font-family="Verdana,Geneva,DejaVu Sans,sans-serif"
+    text-rendering="geometricPrecision"
+    font-size="110"
+  >
+    <text
+      aria-hidden="true"
+      x="185"
+      y="150"
+      fill="#010101"
+      fill-opacity=".3"
+      transform="scale(.1)"
+      textLength="250"
+    >
+      type
+    </text>
+    <text x="185" y="140" transform="scale(.1)" fill="#fff" textLength="250">
+      type
+    </text>
+    <text
+      aria-hidden="true"
+      x="605"
+      y="150"
+      fill="#010101"
+      fill-opacity=".3"
+      transform="scale(.1)"
+      textLength="430"
+    >
+    @coverage@
+    </text>
+    <text x="605" y="140" transform="scale(.1)" fill="#fff" textLength="430">
+    @coverage@
+    </text>
   </g>
 </svg>
 `.trim();
